bfdc3de551167d138949f324ccbea680d02de336
merytpeters <merytpeters@gmail.com>
2025-11-20 17:28:34 +0100

    feat: add jurisdiction model, routes, schemas, and service; update project model to include jurisdictions

---
+++ app/api/modules/v1/jurisdictions/service/jurisdiction_service.py
@@ -0,0 +1,16 @@
+"""Create jurisdiction 
+    A project needs to exist
+    Parent id is set to first jurisdiction under a project
+    Then no more, other jurisdictions can be created without parent  —— Create service
+"""
+
+
+async def get_jurisdiction_by_id():
+    """Finds Jurisdiction by Id"""
+    pass
+
+async def get_jurisdiction_by_name():
+    """Finds Jurisdiction by name"""
+    pass
+
+async
