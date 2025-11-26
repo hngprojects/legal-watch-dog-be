19bef6e483ba4fcffd8c68f11ee75b94be8650b9
merytpeters <merytpeters@gmail.com>
2025-11-26 18:57:58 +0100

    fix: update down_revision to correct migration reference

--- alembic/versions/639ff4afb64a_parent_child_relationship_name_.py
+++ alembic/versions/639ff4afb64a_parent_child_relationship_name_.py
@@ -14,7 +14,7 @@ import sqlmodel
 
 # revision identifiers, used by Alembic.
 revision: str = '639ff4afb64a'
-down_revision: Union[str, Sequence[str], None] = '7900eb2a90aa'
+down_revision: Union[str, Sequence[str], None] = '5ee1506b7eab'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
