3fb1e9a49392bb614036dd2003f0a1e1a7de28b1
SamuelOshin <samuelt.oshin@gmail.com>
2025-12-01 11:34:50 +0100

    chore(migration): update specialist_hires table creation

--- alembic/versions/eba4d89ad4c9_added_billing_plan_model.py
+++ alembic/versions/eba4d89ad4c9_added_billing_plan_model.py
@@ -34,7 +34,12 @@ def upgrade() -> None:
     
     op.execute("DROP INDEX IF EXISTS ix_billing_invoices_stripe_invoice_id")
     op.create_index(op.f('ix_billing_invoices_stripe_invoice_id'), 'billing_invoices', ['stripe_invoice_id'], unique=True)
-    op.add_column('refresh_token_metadata', sa.Column('issued_at', sa.DateTime(timezone=True), nullable=False))
+    
+    op.execute("""
+        ALTER TABLE refresh_token_metadata
+        ADD COLUMN IF NOT EXISTS issued_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+    """)
+    
     op.alter_column('refresh_token_metadata', 'provider_token_exp',
                existing_type=sa.BIGINT(),
                type_=sa.Integer(),
@@ -46,7 +51,11 @@ def upgrade() -> None:
     op.execute("DROP INDEX IF EXISTS ix_refresh_token_metadata_jti")
     op.create_index(op.f('ix_refresh_token_metadata_jti'), 'refresh_token_metadata', ['jti'], unique=True)
     op.create_index(op.f('ix_refresh_token_metadata_id'), 'refresh_token_metadata', ['id'], unique=False)
-    op.drop_column('refresh_token_metadata', 'created_at')
+    
+    op.execute("""
+        ALTER TABLE refresh_token_metadata
+        DROP COLUMN IF EXISTS created_at
+    """)
     # ### end Alembic commands ###
 
 
