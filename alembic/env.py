6c00be2fa9391c3845a43ae8769cfa037a8d8e5a
Precios Ezeigbo <preciousezeigo81@gmail.com>
2025-12-04 23:48:40 +0100

    Add Ticket model to Alembic migrations and fix OTP foreign key constraint

--- alembic/env.py
+++ alembic/env.py
@@ -25,6 +25,7 @@ from app.api.modules.v1.scraping.models.change_diff import ChangeDiff
 from app.api.modules.v1.contact_us.models.contact_us_model import ContactUs
 from app.api.modules.v1.hire_specialists.models.specialist_models import SpecialistHire
 from app.api.modules.v1.notifications.models.revision_notification import Notification
+from app.api.modules.v1.tickets.models.ticket_model import Ticket
 
 
 # this is the Alembic Config object, which provides
