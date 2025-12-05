7e054d724c5787b86a2a4b3261127f17bf06c082
Precios Ezeigbo <preciousezeigo81@gmail.com>
2025-12-05 22:34:47 +0100

    chore: fix migration head to point to latest dev

--- alembic/versions/7810240dba0c_add_change_diff_id_to_tickets.py
+++ alembic/versions/7810240dba0c_add_change_diff_id_to_tickets.py
@@ -1,7 +1,7 @@
 """add_change_diff_id_to_tickets
 
 Revision ID: 7810240dba0c
-Revises: a249039be841
+Revises: f1a2b3c4d5e6
 Create Date: 2025-12-05 13:29:21.255218
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '7810240dba0c'
-down_revision: Union[str, Sequence[str], None] = 'a249039be841'
+down_revision: Union[str, Sequence[str], None] = 'f1a2b3c4d5e6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
